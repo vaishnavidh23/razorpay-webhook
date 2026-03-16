5f8aa491eecee3c7f37b18545d03796b37dc89c6
vaishnavidh23 <vaishnavi.1252100030@vit.edu>
2026-03-16 14:58:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const FIREBASE_URL = "https://vending-machine-3a10d-default-rtdb.firebaseio.com";
+
+app.post("/webhook", async (req, res) => {
+
+    const event = req.body;
+
+    if(event.event === "payment.captured"){
+
+        await fetch(`${FIREBASE_URL}/machine/MACHINE_01.json`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ status: "PAID" })
+        });
+
+        console.log("Payment successful");
+    }
+
+    res.send("Webhook received");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
